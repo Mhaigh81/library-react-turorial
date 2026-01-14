4bad6633756c980929b70e6d6131dc9614e961f4
Mhaigh81 <mhaigh@me.com>
2026-01-14 12:18:12 -0500

    help

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
+import Featured from './components/Featured';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
 import Nav from './components/Nav';
+import Book from './components/ui/Book';
+import Discounted from './components/ui/Discounted';
+
 
 
 function App() {
   return (
     <div className="App">
       <Nav />        
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+
     </div>
   );
 }
