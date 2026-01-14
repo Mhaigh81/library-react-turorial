2b8966d68e6daf64fb85090af27d19f239614bc7
Mhaigh81 <mhaigh@me.com>
2026-01-14 12:25:34 -0500

    full paths on discounted

--- src/components/ui/Discounted.jsx
+++ src/components/ui/Discounted.jsx
@@ -1,24 +1,24 @@
-// import React from "react";
-// import { books } from "../src/data";
-// import Book from "./ui/Book";
+import React from "react";
+import { books } from "/Users/mhaigh/library-react-turorial/src/data"
+import Book from "/Users/mhaigh/library-react-turorial/src/components/ui/Book.jsx";
 
-// const Discounted = () => {
-//   return (
-//     <section id="recent">
-//       <div className="container">
-//         <div className="row">
-//           <div className="section__title">
-//             Discount <span className="purple">Books</span>
-//           </div>
-//           <div className="books">
-//             {books.map((book) => (
-//               <Book book={book} key={book.id} />
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
+const Discounted = () => {
+  return (
+    <section id="recent">
+      <div className="container">
+        <div className="row">
+          <div className="section__title">
+            Discount <span className="purple">Books</span>
+          </div>
+          <div className="books">
+            {books.map((book) => (
+              <Book book={book} key={book.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-// export default Discounted;
+export default Discounted;
