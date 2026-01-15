be868a1fc397fb7ba63e59f0b4e875b6cba0b98c
Mhaigh81 <mhaigh@me.com>
2026-01-15 13:06:59 -0500

    stuck on footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/Landing';
 import Nav from './components/Nav';
 import Book from './components/ui/Book';
 import Discounted from './components/ui/Discounted';
+import Explore from './components/ui/Expolore';
+import Footer from './components/ui/Footer';
 
 
 
@@ -16,7 +18,8 @@ function App() {
       <Highlights />
       <Featured />
       <Discounted />
-
+      <Explore />
+      <Footer />
     </div>
   );
 }
