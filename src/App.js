ed1aa6740523f60abb46a2a8e9e8979581f3f747
Mhaigh81 <mhaigh@me.com>
2026-01-16 12:29:15 -0500

    booksInfo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from './data';
+import BooksInfo from "./pages/BooksInfo"
 
 
 
@@ -17,6 +18,7 @@ function App() {
         <Routes>      
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books books={books} />} />
+          <Route path="/books/1" element={<BooksInfo books={books} />} />
           {/* <Route path="/books/:id" element={<BooksInfo books={books} addToCart={addToCart} cart={cart} />} /> */}
           {/* <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} />} /> */}
         </Routes>   
